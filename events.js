089b76afa3cc63f09ec69dc7415c70eb6dd412b3
michaelrame91-collab <michael.rame91@gmail.com>
2026-06-02 10:15:07 +0200

    Update events.js

--- events.js
+++ events.js
@@ -1,15 +1,17 @@
 /**Ne sers qu'à ajouter des évènements aléatoires pas vraiments importants*/
-
 document.getElementById("gaster-img").addEventListener("click", function () {
 
     let r = Math.random(); /**Génère aléatoirement entre 0 et 0.999999999999999... */
 
-    if (r < 0.001) {
+    if (r < 0.01) {
+        // Changer le fond en tv.gif
+        document.body.style.backgroundImage = "url('tv.gif')";
+        // Ouvrir le lien
         window.open("https://undertale.fandom.com/fr/wiki/W._D._Gaster", "_blank");
         return;
     }
 
-    if (r < 0.02) {
+    if (r < 0.03) {
         window.open("https://undertale.fandom.com/wiki/Sans", "_blank");
         return;
     }
@@ -33,7 +35,7 @@ document.getElementById("gaster-img").addEventListener("click", function () {
         window.open("https://fr.wikipedia.org/wiki/Deltarune", "_blank");
         return;
     }
-    
+
     if (r < 0.71) {
         window.open("Mystère.txt", "_blank");
         return;
